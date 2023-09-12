161aa7f3a8341a549e23bc098825a2585bb1ad52
Roshan Singh <121785689+otroshan@users.noreply.github.com>
2023-09-13 01:34:12 +0530

    added prettier and stylelint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import styles from './page.module.css'
+import styles from './page.module.scss';
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <h1>Hello world!</h1>
     </main>
-  )
+  );
 }
